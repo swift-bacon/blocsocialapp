e2f3d28661b9a55b6e9ab4d9a73990de7d971830
Marcin Pietrzak <pietrzakmarcin@hotmail.com>
2025-08-04 22:39:47 +0200

    Init project

---
+++ android/app/src/main/kotlin/com/swiftbacon/blocsocialapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.swiftbacon.blocsocialapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:blocsocialapp/application.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
